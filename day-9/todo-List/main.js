2cd53a802ca82981a89032ef4591e9f7eabeca25
sachin-me <sachinkumartiwari363@gmail.com>
2018-10-20 22:04:20 +0530

    modified todo-List

--- day-9/todo-List/main.js
+++ day-9/todo-List/main.js
@@ -1,16 +1,26 @@
 let text = document.querySelector('.todo-text');
-let button = document.querySelector('.btn');
 let list = document.querySelector('.todo-list');
 
 
-function addList(event) {
+function addList() {
     var listItem = document.createElement('li');
     if (text.value) {
         listItem.innerText = text.value;
         list.appendChild(listItem);
         text.value = '';
+        listItem.style.listStyleType = "none";
+        listItem.style.color = "darkgrey";
+        listItem.style.fontSize = "1.5em";
+        listItem.style.fontFamily = "Arial, Helvetica, sans-serif"
+    }
+
+}
+
+function eventListen(e) {
+    if (e.code == "Enter") {
+        addList();
     }
 }
 
 
-button.addEventListener("click", addList);
+text.addEventListener("keydown", eventListen);
