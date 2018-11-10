8f5b5bcab7016241071ca6ac39a7c64170b6a861
sachin-me <sachinkumartiwari363@gmail.com>
2018-11-10 23:39:22 +0530

    modify 1.js

--- day-15/HOF & Closure/1.js
+++ day-15/HOF & Closure/1.js
@@ -36,9 +36,6 @@ function map(array, callback) {
 let newMappedArray = map([1, 2, 3], addTwo);
 console.log(newMappedArray);
 
-// console.log(map([1, 2, 3], addTwo));
-
-
 // Challenge 4
 
 function changeStr(arr) {
@@ -59,44 +56,84 @@ console.log(forEach(changeStr(['a', 'b', 'c', 'd'])));
 //--------------------------------------------------
 
 //Extension 1
-function mapWith(array, callback) {
-
+function logNumber(n) {
+  console.log(n);
 }
+
+function mapWith(array, callback) {
+	array.forEach(elem => callback(elem));
+}
+
+mapWith([1, 2, 3], logNumber);
 
 //Extension 2
-function reduce(array, callback, initialValue) {
 
+function add(a, b) {
+  return a + b;
 }
+
+function reduce(array, callback, initialValue) {
+	return array.reduce((accumulator, currentValue) => accumulator + currentValue);
+}
+
+console.log(reduce([4, 1, 3], add, 0));
 
 //Extension 3
-function intersection(arrays) {
 
+function intersection(...arrays) {
+  return arrays.reduce((arr1, arr2) => arr1.filter(x => arr2.indexOf(x) != -1));
 }
+
+console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]));
 
 // console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]));
 // should log: [5, 15]
 
 //Extension 4
-function union(arrays) {
 
+function union(...arrays) {
+	return arrays.reduce((arr1, arr2) => arr1.concat(arr2.filter(item => arr1.indexOf(item) === -1)));
 }
+
+console.log(union([5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]));
 
 // console.log(union([5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]));
 // should log: [5, 10, 15, 88, 1, 7, 100]
 
 //Extension 5
-function objOfMatches(array1, array2, callback) {
 
+function matchObj(str) {
+  return str.toUpperCase();
 }
 
-// console.log(objOfMatches(['hi', 'howdy', 'bye', 'later', 'hello'], ['HI', 'Howdy', 'BYE', 'LATER', 'hello'], function(str) { return str.toUpperCase(); }));
+function objOfMatches(array1, array2, callback) {
+	return array1.reduce((acc, initVal, index) => {
+    if(array2[index] === callback(initVal)) {
+      acc[initVal] = array2[index];
+    }
+    console.log(`acc = ${acc}, initial value = ${initVal}, index = ${index}`);
+    return acc;
+  }, {});
+}
+console.log(objOfMatches(['hi', 'howdy', 'bye', 'later', 'hello'], ['HI', 'HOWDY', 'BYE', 'LATER', 'hello'], matchObj));
+
+// console.log(, function(str) { return str.toUpperCase(); }));
 // should log: { hi: 'HI', bye: 'BYE', later: 'LATER' }
 
-//Extension 6
-function multiMap(arrVals, arrCallbacks) {
 
+//Extension 6
+
+function multiMap(arrVals, arrCallbacks) {
+  return arrVals.reduce((acc, index) => {
+    acc[index] = arrCallbacks.reduce((arr, func) => {
+      arr.push(func(index));
+      return arr;
+    }, []);
+    return acc;
+  }, {});
 }
 
-// console.log(multiMap(['catfood', 'glue', 'beer'], [function(str) { return str.toUpperCase(); }, function(str) { return str[0].toUpperCase() + str.slice(1).toLowerCase(); }, function(str) { return str + str; }]));
+console.log(multiMap(['catfood', 'glue', 'beer'], [function(str) { return str.toUpperCase(); }, function(str) { return str[0].toUpperCase() + str.slice(1).toLowerCase(); }, function(str) { return str + str; }]));
+
 // should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }
 
